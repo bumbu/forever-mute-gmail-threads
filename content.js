4cc7e76632d822850a1595fc9553c705805ba08d
alex bumbu <bmbalex@gmail.com>
2015-11-12 08:06:46 +0000

    Add visual alerts for mutting start, completion and error

--- content.js
+++ content.js
@@ -33,6 +33,41 @@ InboxSDK.load('1', 'SDK_ID').then(function(sdk){
     keyboardShortcutHandle: null
   });
 
+  function threadMute(ev) {
+    var threadId = ev.threadView.getThreadID();
+
+
+
+    chrome.runtime.sendMessage({action: 'mute', threadIds: [threadId], origin: 'content'}, function(response) {
+      console.log(response);
+      if (response && response.success) {
+        sdk.ButterBar.showMessage({
+          text: 'Muting...'
+        , time: 10000
+        , hideOnViewChanged: true
+        , messageKey: 'thread_muted'
+        });
+      } else {
+        sdk.ButterBar.showError({
+          text: 'Not able to starting muting...'
+        , time: 5000
+        , hideOnViewChanged: true
+        , messageKey: 'thread_muted'
+        });
+      }
+    });
+  }
+
+  sdk.Toolbars.registerToolbarButtonForThreadView({
+    title: 'Muter',
+    iconUrl: chrome.extension.getURL('images/mute.png'),
+    section: sdk.Toolbars.SectionNames.METADATA_STATE,
+    onClick: threadMute,
+    hasDropdown: false,
+    // hideFor: whenNoneSelected,
+    keyboardShortcutHandle: null
+  });
+
   // console.log(sdk.User.getEmailAddress)
 
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
@@ -40,8 +75,22 @@ InboxSDK.load('1', 'SDK_ID').then(function(sdk){
 
     if (request.action === 'mute') {
       if (request.success) {
+        sdk.ButterBar.showMessage({
+          text: 'Successfuly muted'
+        , time: 2000
+        , hideOnViewChanged: true
+        , messageKey: 'thread_muted'
+        })
+
         console.log('Successfuly muted', request.threadId)
       } else {
+        sdk.ButterBar.showError({
+          text: 'Unsuccessfuly muted'
+        , time: 5000
+        , hideOnViewChanged: true
+        , messageKey: 'thread_muted'
+        })
+
         console.log('Unsuccessfuly muted', request.threadId)
       }
     }
